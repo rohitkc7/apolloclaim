7288782dff102f2c731267f6682d0bc5c4eb97a9
Rohit K.C <rohitkc7@gmail.com>
2024-09-15 16:58:44 +0545

    branch commit

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Toast from 'react-native-toast-message';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { shadow } from 'react-native-paper';
+
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
